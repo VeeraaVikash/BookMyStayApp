002bb542333572ae028b7252b2e532604f3d8f42
mannangupta813 <mg7157@srmist.edu.in>
2026-04-10 15:04:07 +0530

    UC1

--- App/src/UseCase1HotelBookingApp.java
+++ App/src/UseCase1HotelBookingApp.java
@@ -20,7 +20,8 @@ public class UseCase1HotelBookingApp {
      *
      * @param args command-line arguments (not used in this use case)
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("============================================");
         System.out.println("   Welcome to BookMyStay                   ");
         System.out.println("   Hotel Booking Management System         ");
